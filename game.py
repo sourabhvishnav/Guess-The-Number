b37e60aba75e9ba73e763fc4477614c3abfd19b5
sourabhvishnav <62550282+sourabhvishnav@users.noreply.github.com>
2021-05-31 07:42:36 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,18 @@
+import random
+
+randomNumber = random.randint(1,9)
+print(randomNumber)
+chances = 0
+inputNumber = int(input('Enter A Number : '))
+
+while chances < 5:
+    if inputNumber == randomNumber :
+        print('You were Correct !')
+        break
+
+    chances = chances + 1
+    
+    if not chances <  5:
+        print('You lose ! It was ' , randomNumber)
+
+    
